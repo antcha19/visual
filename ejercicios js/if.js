e85b3743cc95d73a8f58bf878846eddac1b1ffd3
antcha19 <yepez261@gmail.com>
2020-12-05 20:44:50 +0100

    ejer

---
+++ ejercicios js/if.js
@@ -0,0 +1,8 @@
+var price = 65;
+if (price <50) {
+console.log ("This is cheap!");
+} else if (price <100) {
+console.log ("This is not cheap ...");
+} else {
+console.log ("This is expensive!");
+}
---
+++ ejercicios js/let.js
@@ -0,0 +1,9 @@
+let name = "Nacho";
+let age = 40;
+
+if (2> 1)
+{
+let name = "Nacho";
+console.log ("Name inside:", name);
+}
+console.log ("Name out:", name);
