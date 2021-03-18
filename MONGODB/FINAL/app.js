8bd2548a0dc4fbe9057d35ba739b23c4eea453a3
antcha19 <55527269+antcha19@users.noreply.github.com>
2021-03-18 10:51:57 +0100

    chf

--- MONGODB/FINAL/app.js
+++ MONGODB/FINAL/app.js
@@ -1,8 +1,7 @@
-const { error } = require("console");
 const fs = require("fs")
 
 let salir = document.getElementById("idsalir");
-let PaginaProductos =document.getElementById("paginaproductos");
+let PaginaProductos = document.getElementById("paginaproductos");
 let PaginaClientes = document.getElementById("paginaclientes");
 let PaginaCompra = document.getElementById("paginacompra");
 let PaginaDevolucion = document.getElementById("paginadevolucion");
@@ -70,6 +69,7 @@ let clientesSchema = new mongoose.Schema({
 //añade una tabla cliente a la bbdd 
 let clientemodelo = mongoose.model('cliente', clientesSchema);
 mostrartodosclientes();
+limpiarclientes();
 
 //anadir clientes
 document.getElementById("btnCargar").addEventListener('click', () => {
@@ -98,9 +98,10 @@ document.getElementById("btnCargar").addEventListener('click', () => {
     //
     Promise.all([p1]).then(values => {
         mongoose.connection.close();
-      
+
     });
     mostrartodosclientes();
+    limpiarclientes();
 })
 
 
@@ -130,31 +131,31 @@ function mostrartodosclientes() {
                         </tr>
                     </table> 
                 </div>`;
-               
+
         });
         document.getElementById("wrapper").innerHTML = cadenaDOM;
     }).catch(error => {
         console.log("ERROR en find");
         alert('Error al buscar');
     });
-   
+
 }
 //borrar cliente
-document.getElementById("btnborrar").addEventListener('click',()=>{
+document.getElementById("btnborrar").addEventListener('click', () => {
     let txtborrarcliente = document.getElementById('txtborrarcliente').value;
-    clientemodelo.remove({dni: txtborrarcliente}).then(result =>{
+    clientemodelo.remove({ dni: txtborrarcliente }).then(result => {
         alert('El cliente ha sido borrado');
-    }).catch(error =>{
+    }).catch(error => {
         alert('Error al borrar el cliente')
     });
     mostrartodosclientes();
+    limpiarclientes();
 })
 
-//
 //buscar por dni
-document.getElementById("btnbuscardni").addEventListener('click',()=>{
+document.getElementById("btnbuscardni").addEventListener('click', () => {
     let txtbuscardni = document.getElementById("txtbuscardni").value;
-    clientemodelo.find({dni:{$regex:'.*'+txtbuscardni+'.*'}}).then(resultado => {
+    clientemodelo.find({ dni: { $regex: '.*' + txtbuscardni + '.*' } }).then(resultado => {
         let cadenaDOM = "";
         resultado.forEach(cliente => {
             cadenaDOM +=
@@ -180,10 +181,48 @@ document.getElementById("btnbuscardni").addEventListener('click',()=>{
                 </div>`;
         });
         document.getElementById("wrapper").innerHTML = cadenaDOM;
-        
+
     }).catch(error => {
         alert('Error al buscar el Cliente, puede que no exista')
-    }); 
+    });
+    limpiarclientes();
+    
+})
+
+//buscar por nombre del cliente
+document.getElementById("btnbuscarnombre").addEventListener('click', () => {
+    let txtBuscarNombre = document.getElementById("txtbuscarnombre").value;
+    clientemodelo.find({ nombre: { $regex: '.*' + txtBuscarNombre + '.*' } }).then(resultado => {
+        let cadenaDOM = "";
+        resultado.forEach(cliente => {
+            cadenaDOM +=
+                `<div>
+                    <table >
+                        <tr style="background-color:#567CE3 ;">
+                            <th>DNI</th>
+                            <th>Nombre</th>
+                            <th>Apellidos</th>
+                            <th>Edad</th>
+                            <th>Direccion</th>
+                            <th>Email</th>
+                        </tr>
+                        <tr>
+                            <td>${cliente.dni}</td>
+                            <td>${cliente.nombre}</td>
+                            <td>${cliente.apellidos}</td>
+                            <td>${cliente.edad}</td>
+                            <td>${cliente.direccion}</td>
+                            <td>${cliente.email}</td>
+                        </tr>
+                    </table> 
+                </div>`;
+        });
+        document.getElementById("wrapper").innerHTML = cadenaDOM;
+    }).catch(error => {
+        alert('Error al buscar el nombre del cliente, puede que no exista')
+    });
+    
+    limpiarclientes();
 })
 
 
@@ -195,19 +234,39 @@ salir.addEventListener('click', (event) => {
 
 //cambia de pagina a productos.html
 PaginaProductos.addEventListener('click', (event) => {
-    document.location.href= "productos.html";
+    document.location.href = "productos.html";
 })
 
 //cambia de pagina a compra.html
-PaginaCompra.addEventListener('click', (event)  =>{
-    document.location.href="compra.html";
+PaginaCompra.addEventListener('click', (event) => {
+    document.location.href = "compra.html";
 })
 
-//cambia de pagina
+//cambia de pagina a devolucion.html
 PaginaDevolucion.addEventListener('click', (event) => {
-    document.location.href="devolucion.html";
+    document.location.href = "devolucion.html";
 })
 
 
+//actualizar la pagina
+PaginaClientes.addEventListener('click', () => {
+    mostrartodosclientes();
+    limpiarclientes();
+})
 
 
+function limpiarclientes() {
+    //insertar
+    document.getElementById('txtnuevodni').value = "";
+    document.getElementById('txtnuevonombre').value = "";
+    document.getElementById('txtnuevoapellidos').value = "";
+    document.getElementById('txtnuevoedad').value = "";
+    document.getElementById('txtnuevodireccion').value = "";
+    document.getElementById('txtnuevoemail').value= "";
+    //buscar el nombre
+    document.getElementById("txtbuscarnombre").value = "";
+    //buscar el dni
+    document.getElementById("txtbuscardni").value = "";
+    //borrar por el dni
+    document.getElementById('txtborrarcliente').value = " ";
+}
